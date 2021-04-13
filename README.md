d67b10ce038ea9f134fee5f735b4051fa0d0536a
Aaron40hjkh <73937161+Aaron40hjkh@users.noreply.github.com>
2021-04-13 13:39:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Air-Balloon
-n
+# afa156b839e1b81fbfa9bf61b99ed6eb
---
+++ sketch.js
@@ -0,0 +1,77 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+   balloon.velocityX = -3 ;
+   balloon.velocityY = 0;
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.velocityX = 3 ;
+   balloon.velocityY = 0;
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.velocityX = 0 ;
+    balloon.velocityY = -3;
+    
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.velocityX = 0 ;
+   balloon.velocityY = 3;
+  }
+    function updateHeight(x,y){
+      database.ref('balloon/height').set({
+      'x':height.x+x,
+      'y':height.y+y
+      })
+    }
+
+    function readHeight(data){
+      height = data.val();
+      balloon.x = height.x;
+      balloon.y = height.y;
+    }
+    function showError(){
+    console.log("Error in writing to the database");
+
+
+
+
+    }
+
+
+
+    
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
